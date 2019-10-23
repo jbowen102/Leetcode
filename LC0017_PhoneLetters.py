30b08d2263fb488e0601280698c3aeb04a45a0a7
Jonathan Bowen <jbowen@posteo.net>
2019-10-22 20:37:55 -0400

    Finish 17

--- LC0017_PhoneLetters.py
+++ LC0017_PhoneLetters.py
@@ -17,21 +17,32 @@ Although the above answer is in lexicographical order, your answer could be in
 any order you want.
 """
 
-class Solution:
+class Solution(object):
     # def letterCombinations(self, digits: str) -> List[str]:
 
-    def init(self):
-        self.key_map = {2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno',
-                        7: 'pqrs', 8: 'tuv', 9: 'wxyz'}
-        self.output_list = []
+    def __init__(self):
+        self.key_map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+                        '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
 
     def letterCombinations(self, digits):
-        if not digits:
-            return ''
+        output_list = []
+        # print('digits: %s' % digits)
 
-        else:
-            for letter in self.key_map[digits[0]]:
-                self.output_list.append(letter + self.letterCombinations(digits[1:]))
+        if digits:
+            # recurse and get all combinations of letters later in the sequence.
+            lower_level_list = self.letterCombinations(digits[1:])
 
+            letters = self.key_map[digits[0]]
+            for letter in letters:
+                # print('\tletter %s from set %s' % (letter, letters))
+                if lower_level_list:
+                    for existing_string in lower_level_list:
+                        # print('\tadding %s to string %s' % (letter, existing_string))
+                        output_list.append(letter + existing_string)
+                else:
+                    # print('\tappending letter %s by itself' % letter)
+                    output_list.append(letter)
 
-        return self.output_list
+        return output_list
+
+# Successfully submitted
