e1a1ffa9cb91aabf7341688c979b4fbb47da9abf
Jonathan Bowen <jbowen@posteo.net>
2019-11-21 22:48:59 -0500

    Get 16 to run, but is too slow.

--- LC0016_3SumClose2.py
+++ LC0016_3SumClose2.py
@@ -30,11 +30,57 @@ class Solution:
                 nums3 = nums2.copy()
                 nums3.remove(num2)
 
-                # perform binary search
-                # pick mid-point (floor div).
-                # if mid-point larger than target3, recurse on left half.
-                # base case: only one item in search space.
+                num3 = self.bin_search(target3, nums3)
 
-# build algorithm that sorts input O(nlogn), then iterates through each item in
-# outer loop then each item in inner loop (O(n^2)), then for final target val, use binary
-# search (O(logn)). Overall alg O(nlogn + n^2 + logn) = O(n^2).
+                new_sum = num + num2 + num3
+
+                if abs(new_sum - target) < abs(best_sum - target):
+                    best_sum = new_sum
+
+        return best_sum
+
+
+    def bin_search(self, target, array):
+        # print(array)
+        # base case
+        if len(array) == 1:
+            return array[0]
+        elif len(array) == 2:
+            if abs(target - array[0]) < abs(target - array[1]):
+                return array[0]
+            else:
+                return array[1]
+
+
+        midpoint = int(len(array) / 2)
+        # second sort-of base case. If list split and target is between end of
+        # first list and beginning of second, then one of them is the closest.
+        if target > array[midpoint] and target < array[midpoint+1]:
+            if abs(target - array[midpoint]) < abs(target - array[midpoint+1]):
+                return array[midpoint]
+            else:
+                return array[midpoint + 1]
+
+        # another base case
+        if target >= array[-1]:
+            return array[-1]
+        elif target <= array[0]:
+            return array[0]
+
+        # standard binary search
+        if target > array[midpoint]:
+            # recurse on second half
+            return self.bin_search(target, array[midpoint:])
+        elif target < array[midpoint]:
+            # recurse on first half
+            return self.bin_search(target, array[:midpoint])
+        elif target == array[midpoint]:
+            return array[midpoint]
+
+
+# time limit exceeded. Passed 74 of 125 test cases.
+
+
+# sorts input O(nlogn), then iterates through each item in outer loop
+# then each item in inner loop (O(n^2)), then for final target val, use binary
+# search (O(logn)). Overall alg O(nlogn + n^2*logn) = O(n^2).
